4e17122375f658f2c8c2c2fc367d0278f073d27c
Niall <niall.i.campbell@gmail.com>
2018-09-02 15:11:21 +0100

    first commit

--- load.py
+++ load.py
@@ -5,7 +5,7 @@ import os
 
 header = '''<html>
     <head>
-        <title>'Duro Results</title>
+        <title>XDuro Results</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     </head>
@@ -31,32 +31,39 @@ def runPoll(event, context):
     client.access_token = os.environ.get('strava_access_token', '')
     segments = [('XDS1',15888276), ('XDS2',15781529), ('XDS3',15781535), ('XDS4',15781545), ('XDS5',15781550)]
     riders = {}
+    maxTimes = {}
+    pullResults(client, maxTimes, riders, segments)
+    html = formatResults(maxTimes, riders, segments)
+    toS3(html)
 
+def pullResults(client, maxTimes, riders, segments):
     for club in [238976]:
         leaderboards = {}
-        maxTimes = {}
         for _, segment in segments:
-            leaderboards[segment] = client.get_segment_leaderboard(segment, timeframe='this_week', top_results_limit=50, club_id=club)
+            leaderboards[segment] = client.get_segment_leaderboard(segment, timeframe='this_week', top_results_limit=50,
+                                                                   club_id=club)
             maxTimes[segment] = timedelta(0)
         for segment, leaderBoard in leaderboards.items():
             for effort in leaderBoard.entries:
                 if '2018-09-0' not in str(effort.start_date):
                     pass
-                riderEfforts = riders.get( effort.athlete_name, {} )
-                riderEfforts[ segment ] = effort.elapsed_time
-                riders[ effort.athlete_name ] = riderEfforts
+                riderEfforts = riders.get(effort.athlete_name, {})
+                riderEfforts[segment] = effort.elapsed_time
+                riders[effort.athlete_name] = riderEfforts
                 if effort.elapsed_time > maxTimes[segment]:
                     maxTimes[segment] = effort.elapsed_time
 
+
+def formatResults(maxTimes, riders, segments):
     resultRows = {}
     for rider, results in riders.items():
         gotAllResults = True
         totalTime = timedelta(0)
-        thisRow = '<tr><th scope=\'row\'>{}</th>'.format( rider )
+        thisRow = '<tr><th scope=\'row\'>{}</th>'.format(rider)
         for _, segment in segments:
-            result = results.get( segment, maxTimes[segment] )
+            result = results.get(segment, maxTimes[segment])
             gotAllResults = gotAllResults and segment in results
-            totalTime = ( totalTime + result )
+            totalTime = (totalTime + result)
             if segment in results:
                 thisRow += '<td>{}</td>'.format(result)
             else:
@@ -65,16 +72,16 @@ def runPoll(event, context):
             thisRow += '<td>{}</td></tr>'.format(totalTime)
         else:
             thisRow += '<td><i>{}</i></td></tr>'.format(totalTime)
-        resultRows[ totalTime  ] = thisRow
-
-
+        resultRows[totalTime] = thisRow
     html = header
     for segment in segments:
-        html += '<th scope="col"><a href="https://www.strava.com/segments/{}">{}</a></th>'.format(segment[1], segment[0])
+        html += '<th scope="col"><a href="https://www.strava.com/segments/{}">{}</a></th>'.format(segment[1],segment[0])
     html += mid
     for result in sorted(resultRows.keys()):
         html += resultRows[result]
     html += footer
+    return html
 
+def toS3(html):
     s3 = boto3.resource('s3')
     s3.Object('bikerid.es', 'xduro/index.html').put(Body=html, ContentType='text/html')
