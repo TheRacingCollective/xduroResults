0267ee5c8aea7fb0de18063d25948fdf014e934d
Niall Campbell <nialliain@users.noreply.github.com>
2018-09-07 21:11:40 +0100

    Update load.py

--- load.py
+++ load.py
@@ -5,26 +5,28 @@ import os
 import pytz
 
 header = '''<html>
-    <head>
-        <title>PennDURO Results</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    </head>
-    <body>
+  <head>
+    <title>XDURO Results</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  </head>
+  <body>
     <table class="table table-striped">
-  <thead>
-    <tr>
-          <th scope="col">Rider</th>'''
-mid = '''      <th scope="col">Total</th>
-    </tr>
-  </thead>
-  <tbody>'''
-footer = '''  </tbody>
-</table>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-    </body>
+      <thead>
+        <tr>
+          <th scope="col">Rider</th>
+'''
+mid = '''          <th scope="col">Total</th>
+        </tr>
+      </thead>
+      <tbody>
+'''
+footer = '''      </tbody>
+    </table>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
 </html>'''
 
 def runPoll(event, context):
@@ -46,7 +48,7 @@ def pullResults(maxTimes, riders, segments):
             maxTimes[segment] = timedelta(0)
         for segment, leaderBoard in leaderboards.items():
             for effort in leaderBoard.entries:
-                if datetime(2018, 10, 6, tzinfo=pytz.utc) > effort.start_date > datetime(2018, 10, 6, 23, 59, tzinfo=pytz.utc):
+                if effort.start_date < datetime(2018, 10, 6, tzinfo=pytz.utc) or effort.start_date > datetime(2018, 10, 6, 23, 59, tzinfo=pytz.utc):
                     continue
                 riderEfforts = riders.get(effort.athlete_name, {})
                 riderEfforts[segment] = effort.elapsed_time
@@ -54,33 +56,32 @@ def pullResults(maxTimes, riders, segments):
                 if effort.elapsed_time > maxTimes[segment]:
                     maxTimes[segment] = effort.elapsed_time
 
-
-
 def formatResults(maxTimes, riders, segments):
     resultRows = {}
     for rider, results in riders.items():
         gotAllResults = True
         totalTime = timedelta(0)
-        thisRow = '<tr><th scope=\'row\'>{}</th>'.format(rider)
+        thisRow = '        <tr>\n          <th scope=\'row\'>{}</th>\n'.format(rider)
         for _, segment in segments:
             result = results.get(segment, maxTimes[segment])
             gotAllResults = gotAllResults and segment in results
             totalTime = (totalTime + result)
             if segment in results:
-                thisRow += '<td>{}</td>'.format(result)
+                thisRow += '          <td>{}</td>\n'.format(result)
             else:
-                thisRow += '<td><i>{}</i></td>'.format(result)
+                thisRow += '          <td><i>{}</i></td>\n'.format(result)
         if gotAllResults:
-            thisRow += '<td>{}</td></tr>'.format(totalTime)
+            thisRow += '          <td>{}</td></tr>\n'.format(totalTime)
         else:
-            thisRow += '<td><i>{}</i></td></tr>'.format(totalTime)
-        resultRows[totalTime] = thisRow
+            thisRow += '          <td><i>{}</i></td></tr>\n'.format(totalTime)
+        k = str(totalTime).zfill(24*60*60) + rider
+        resultRows[k] = thisRow
     html = header
     for segment in segments:
-        html += '<th scope="col"><a href="https://www.strava.com/segments/{}">{}</a></th>'.format(segment[1],segment[0])
+        html += '          <th scope="col"><a href="https://www.strava.com/segments/{}">{}</a></th>\n'.format(segment[1],segment[0])
     html += mid
-    for result in sorted(resultRows.keys()):
-        html += resultRows[result]
+    for k in sorted(resultRows.keys()):
+        html += resultRows[k]
     html += footer
     return html
 
