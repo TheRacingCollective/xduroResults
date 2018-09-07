4c57f1fdacf3e5b80f460646c36eec2fa3516a30
Niall Campbell <nialliain@users.noreply.github.com>
2018-09-07 21:00:53 +0100

    Update load.py

--- load.py
+++ load.py
@@ -1,12 +1,12 @@
 from stravalib.client import Client
-from datetime import timedelta
+from datetime import datetime, timedelta
 import boto3
 import os
 import pytz
 
 header = '''<html>
     <head>
-        <title>XDURO Results</title>
+        <title>PennDURO Results</title>
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     </head>
