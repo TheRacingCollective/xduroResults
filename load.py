81ccd46e313e6a0f13421f356f50e6a3230eab11
Niall Campbell <nialliain@users.noreply.github.com>
2018-10-02 23:28:59 +0100

    Update load.py

--- load.py
+++ load.py
@@ -78,33 +78,34 @@ class XduroResultBuilder(object):
 
 class ResultsPrinter(object):
 
-    header = '''<html>
-      <head>
-        <title>XDURO Results</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-      </head>
-      <body>
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th scope="col">Rider</th>
+    header = '''<!DOCTYPE html>
+<html>
+  <head>
+    <title>XDURO Results</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  </head>
+  <body>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Rider</th>
     '''
 
-    mid = '''          <th scope="col">Total</th>
-            </tr>
-          </thead>
-          <tbody>
+    mid = '''        <th scope="col">Total</th>
+        </tr>
+      </thead>
+      <tbody>
     '''
 
-    footer = '''      </tbody>
-        </table>
-        <i>Classement g&eacute;n&eacute;ral - provisoire</i>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-      </body>
-    </html>'''
+    footer = '''    </tbody>
+    </table>
+    <i>Classement provisoire</i>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
+</html>'''
 
     def format_html(self, segments, results):
         html = self.header
