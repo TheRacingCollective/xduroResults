0a0420a8a308c75525b9fd0300c3d6c95e239df5
Niall Campbell <nialliain@users.noreply.github.com>
2018-10-04 08:48:45 +0100

    Update load.py

--- load.py
+++ load.py
@@ -1,5 +1,6 @@
 from stravalib.client import Client
-from datetime import timedelta
+from datetime import datetime, timedelta
+import pytz
 import os
 
 def runPoll(event, context):
@@ -25,7 +26,7 @@ class XduroResultBuilder(object):
         self.client = Client()
         self.client.access_token = access_token
         self.club = 238976
-        self.timeFrame = 'today'
+        self.timeFrame = 'this_month'
         self.min_results = 2
 
     def get_results_for(self, segments):
@@ -78,34 +79,33 @@ class XduroResultBuilder(object):
 
 class ResultsPrinter(object):
 
-    header = '''<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>XDURO Results</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-  </head>
-  <body>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th scope="col">Rider</th>
-'''
+    header = '''<html>
+      <head>
+        <title>XDURO Results</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+      </head>
+      <body>
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Rider</th>
+    '''
 
-    mid = '''        <th scope="col">Total</th>
-        </tr>
-      </thead>
-      <tbody>
-'''
+    mid = '''          <th scope="col">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+    '''
 
-    footer = '''    </tbody>
-    </table>
-    <i>Classement provisoire</i>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-  </body>
-</html>'''
+    footer = '''      </tbody>
+        </table>
+        <i>Classement provisoire, &agrave; {}</i>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+      </body>
+    </html>'''
 
     def format_html(self, segments, results):
         html = self.header
@@ -119,5 +119,6 @@ class ResultsPrinter(object):
                 html += format_string.format(result[segId][0])
             format_string = '          <td>{}</td>\n' if result['total'][1] else '          <td><i>{}</i></td>\n'
             html += format_string.format(result['total'][0])
-        html += self.footer
+        update_time = '{:%H:%M}'.format(datetime.utcnow().replace(tzinfo=pytz.timezone('Europe/London')))
+        html += self.footer.format(update_time)
         return html
